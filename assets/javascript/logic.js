adfd414b993e28a2b4a7e54a3d8bad4e51cb4c5d
ichumats <ivanna.chumatschko22@gmail.com>
2019-11-13 12:32:33 -0700

    incorporated moment.js functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,9 +21,8 @@ $(document).ready(function() {
     //Initial values
     var trainName = '';
     var destination = '';
-    var firstTrain = 0;
+    var firstTrain = '';
     var frequency = 0;
-    var now = moment()._d;
 
   //FUNCTIONS-------------------------------------------------------------------------------------------------------------------------------------
 
@@ -35,7 +34,7 @@ $(document).ready(function() {
       // Grab values from text boxes
       trainName = $('#trainName').val().trim();
       destination = $('#destination').val().trim();
-      firstTrain = $('#firstTrain').val().trim().replace(':', '');
+      firstTrain = $('#firstTrain').val().trim();
       frequency = $('#frequency').val().trim();
       // Code for handling the push
       database.ref().push({
@@ -51,17 +50,39 @@ $(document).ready(function() {
     database.ref().on("child_added", function(childSnapshot) {
       // storing the snapshot.val() in a variable for convenience
       var sv = childSnapshot.val();
+      //Create a variable to hold the moment the first train leaves
+      var cFirstTrain = moment((sv.firstTrain), 'HH:mm')
+      //Create a variable to hold the current moment 
+      var now = moment().format('HH:mm');
+
+      // If the first train hasn't left yet, display sv.firstTRain in the nextArrival space and calculate minutes away based on the difference between now and the time when the train starts running
+        if (now < cFirstTrain._i) {
+          var minutesAway = (moment().diff(moment(cFirstTrain), 'minutes'))/-1;
+          var nextArrival = moment(cFirstTrain);
+          
+        } else { 
+          //Create a variable to hold the difference between the current time and the time the train started running (in minutes)
+          var timeDiff = moment().diff(moment(cFirstTrain), 'minutes');
+          //Create a variable to hold the remainder of dividing timeDiff by the frequency
+          var tRemainder = timeDiff % (sv.frequency);
+          //Create a variable to calculate and hold the minutes away
+          var minutesAway = (sv.frequency) - tRemainder
+          //Create a variable to hold the next arrival time based on the current time and minutesAway
+          var nextArrival = moment().add(minutesAway, 'minutes')
+        }
 
       // Console.log the last user's data
       console.log(sv.trainName);
       console.log(sv.destination);
       console.log(sv.firstTrain);
       console.log(sv.frequency);
+      console.log(cFirstTrain);
       console.log(now);
-
-      
-      // full list of items to the well
-      $('tbody').append("<tr id='trainData'> <td id='nameData'> " +
-        sv.trainName + " </td><td id='destinationData'> " + sv.destination + "</td><td id='frequencyData'> " + sv.frequency + "</td></tr>");
-      });
+      console.log(minutesAway);
+      console.log(nextArrival);
+  
+      // full list of items to the table
+      $('tbody').append(" <tr id='trainData'> <td id='nameData'> " +
+        sv.trainName + " </td><td id='destinationData'> " + sv.destination + " </td><td id='frequencyData'> " + sv.frequency + " </td><td id='nextArrival'> " + moment(nextArrival).format('HH:mm') + " </td><td id='minutesAway'> " + minutesAway + " </td></tr> ");
+    });
 });
