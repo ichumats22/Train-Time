1586140044c716b0aeaf48b081e221b3ec163e4c
ichumats <ivanna.chumatschko22@gmail.com>
2019-11-13 10:23:04 -0700

    added button and database functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,23 +16,48 @@ $(document).ready(function() {
     //Crate a variable to reference the database
     var database = firebase.database();
 
-    var test = 0;
+    //Initial values
+    var trainName = '';
+    var destination = '';
+    var firstTrain = 0;
+    var frequency = 0;
+
   //FUNCTIONS-------------------------------------------------------------------------------------------------------------------------------------
 
   //LOGIC-----------------------------------------------------------------------------------------------------------------------------------------
-    //Testing links 
-    $('#testButton').on('click', function(event) {
+    //Capture user input on button click
+    $('#submitButton').on('click', function(event) {
       event.preventDefault();
       
-      console.log('Links working properly');
-     
-      // Grabbed values from text boxes
-      test = $('#test').val().trim();
-
+      // Grab values from text boxes
+      trainName = $('#trainName').val().trim();
+      destination = $('#destination').val().trim();
+      firstTrain = $('#firstTrain').val().trim().replace(':', '');
+      frequency = $('#frequency').val().trim();
       // Code for handling the push
       database.ref().push({
-        test,
+        trainName,
+        destination,
+        firstTrain,
+        frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
     });
+
+    // Firebase watcher .on("child_added"
+    database.ref().on("child_added", function(childSnapshot) {
+      // storing the snapshot.val() in a variable for convenience
+      var sv = childSnapshot.val();
+
+      // Console.log the last user's data
+      console.log(sv.trainName);
+      console.log(sv.destination);
+      console.log(sv.firstTrain);
+      console.log(sv.frequency);
+
+      
+      // full list of items to the well
+      $('tbody').append("<tr id='trainData'> <td id='nameData'> " +
+        sv.trainName + " </td><td id='destinationData'> " + sv.destination + "</td><td id='frequencyData'> " + sv.frequency + "</td></tr>");
+      });
 });
